7ae34c1bbe2369869f60fad21f0deedd38edb131
Chetan Agarwal <chetan.mouselangelo@gmail.com>
2025-02-14 19:37:07 +0100

    Support GQL playground + update Readme

--- server/README.md
+++ server/README.md
@@ -7,6 +7,22 @@ This is a simple [GraphQL](https://graphql.org/) server implementation using [Ap
 - `npm install`
 - `npm start`
 
+## GraphQL
+
+Once the server starts, the GraphQL API is available on http://localhost:4000/graphql
+
+You can test your queries and mutations in GraphQL Playground by visiting the [endpoint](http://localhost:4000/graphql) in your browser or you can use [Apollo Sandbox](https://studio.apollographql.com/sandbox/explorer)
+
+### Queries
+
+A simple Hello, World! query. Just to make sure your server is up and running
+
+```gql
+query HelloWorld {
+    hello
+}
+```
+
 ## Linting
 
 This repo contains a basic linting setup using [eslint](https://eslint.org/) and [prettier](https://prettier.io/).
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,25 @@
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
 import { typeDefs, resolvers } from "./graphql";
 
 const app: Application = express();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 const PORT = 4000;
 
 async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app, path: "/graphql" });
+    await server.start();
+    server.applyMiddleware({ app, path: "/graphql" });
 
-  app.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${PORT}/graphql`)
-  );
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/graphql`));
 }
 
 startServer();
