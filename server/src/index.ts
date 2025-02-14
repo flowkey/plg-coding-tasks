c2b10b552065bba77c1d5a3ee8b160873b7fd066
Chetan Agarwal <chetan.mouselangelo@gmail.com>
2025-02-14 19:04:38 +0100

    Setup basic GQL server

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Application } from "express";
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
+import { typeDefs, resolvers } from "./graphql";
+
+const app: Application = express();
+app.use(cors());
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const PORT = 4000;
+
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}/graphql`)
+  );
+}
+
+startServer();
